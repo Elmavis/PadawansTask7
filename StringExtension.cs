b4ec3eeec7bdf9b182ceb33d5f45f8c2f6c73e6c
Elmavis <37665679+Elmavis@users.noreply.github.com>
2019-02-17 16:05:42 +0300

    1

--- StringExtension.cs
+++ StringExtension.cs
@@ -1,13 +1,26 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace PadawansTask7
 {
+    class Comp : IComparer<string>
+    {
+        public int Compare(string x, string y)
+        {
+            return x.Length - y.Length;
+        }
+    }
+
     public class StringExtension
     {
         public static void OrderStringsByLength(string[] array)
         {
-            // put your code here
-            throw new NotImplementedException();
+            if (array == null)
+                throw new ArgumentNullException();
+            if (array.Length == 0)
+                throw new ArgumentException();
+            Array.Sort(array, new Comp());
         }
     }
 }
